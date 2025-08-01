4e1e8a59e13bf5393ab08c15b72f8fa57634c7c2
slowww. <47158619+slowwdown@users.noreply.github.com>
2025-08-01 10:22:25 +0200

    feat: implement fivem compatiblity

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'bodacious'
+game 'gta5'
+author 'slowwdown'
+description 'SLoading - A loading screen for FiveM'
+
+files {"config.js", "index.html", "assets/**/*.*"}
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
